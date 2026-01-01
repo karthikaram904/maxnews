1aed889ac6767c480da99758dbf5a95b4858d02e
Karthika <karthikad3@gmail.com>
2026-01-01 08:01:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 (function ($) {
     "use strict";
     
-    // Dropdown on mouse hover
     $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
+  function toggleNavbarMethod() {
+    if ($(window).width() > 1199) {
+      $('.navbar .dropdown')
+        .on('mouseover', function () {
+          $(this).find('.dropdown-toggle').dropdown('show');
+        })
+        .on('mouseout', function () {
+          $(this).find('.dropdown-toggle').dropdown('hide');
+        });
+    } else {
+      $('.navbar .dropdown').off('mouseover mouseout');
+    }
+  }
+  toggleNavbarMethod();
+  $(window).resize(toggleNavbarMethod);
+});
+
     
     
     // Back to top button
